91fa5b068b84578dd383b3fddbc1283edd7943b9
jamaalhassan <kabarojamaal@gmail.com>
2021-02-06 16:11:59 +0000

    Added:CartItems in to Component,CartTotal Components.

--- amazon-cart/src/App.js
+++ amazon-cart/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
-import  Header  from './Header';
+import CartItems from './components/CartItems';
+import CartTotal from './components/CartTotal';
+import  Header  from './components/Header';
 
 function App() {
   return (
   <div className="App">
     <Header title='Amazon Cart'/>
+    <div className="App-main">
+      <CartItems/>
+      <CartTotal/>
+     
+    </div>
   </div>
+  
   );
 }
 export default App;
---
+++ amazon-cart/src/components/CartTotal.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import './CartTotal.css';
+
+function CartTotal() {
+    return (
+      <div className="CartTotal">
+        <h1>Total Cart</h1>
+      </div>
+    )
+}
+
+export default CartTotal
