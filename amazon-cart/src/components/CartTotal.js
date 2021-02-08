5909dd69a73885056a40720019492a066a39d90e
jamaalhassan <kabarojamaal@gmail.com>
2021-02-08 22:57:51 +0000

    Added: Calcul Total Items

--- amazon-cart/src/components/CartTotal.js
+++ amazon-cart/src/components/CartTotal.js
@@ -3,10 +3,21 @@
 import React from 'react'
 import './CartTotal.css';
 
-function CartTotal() {
+function CartTotal({items}) {
+  const TotalPrice = () => {
+   let total = items.map((item,index) => + item.price * item.quantity)
+   let count = Math.round( total.reduce((a, b) => a + b,) * 100)/100;
+   console.log(count)
+    return count;
+  }
     return (
       <div className="CartTotal">
-        <h1>Total Cart</h1>
+        <h1>Subtotal ({items.length} Items):
+          <span className="CarTotal-price">
+            ${TotalPrice()}
+          </span>
+          <button>Procced Check out</button>
+        </h1>
       </div>
     )
 }
