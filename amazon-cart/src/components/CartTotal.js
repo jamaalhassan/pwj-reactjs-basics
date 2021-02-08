f43469abd93deb4c8307509ae19f58333b005b9b
jamaalhassan <kabarojamaal@gmail.com>
2021-02-08 23:34:29 +0000

    Added: CountQty Func

--- amazon-cart/src/components/CartTotal.js
+++ amazon-cart/src/components/CartTotal.js
@@ -7,12 +7,18 @@ function CartTotal({items}) {
   const TotalPrice = () => {
    let total = items.map((item,index) => + item.price * item.quantity)
    let count = Math.round( total.reduce((a, b) => a + b,) * 100)/100;
-   console.log(count)
     return count;
   }
+  const CartQty = () =>{
+    let itemsQty = items.map((item,index) => + item.quantity)
+    let countQty = Math.round( itemsQty.reduce((a, b) => a + b,) * 100)/100;
+    console.log(countQty)
+    return countQty;
+  }
+
     return (
       <div className="CartTotal">
-        <h1>Subtotal ({items.length} Items):
+        <h1>Subtotal ({CartQty()} Items):
           <span className="CarTotal-price">
             ${TotalPrice()}
           </span>
