f3ea006598c955d30b01fab196b24d8b5b947d4c
jamaalhassan <kabarojamaal@gmail.com>
2021-02-09 01:05:09 +0000

    Used:React Nmber Format on Total Price

--- amazon-cart/src/components/CartTotal.js
+++ amazon-cart/src/components/CartTotal.js
@@ -2,13 +2,19 @@
 
 import React from 'react'
 import './CartTotal.css';
+import NumberFormat from 'react-number-format';
 
 function CartTotal({items}) {
   const TotalPrice = () => {
-   let total = items.map((item,index) => + item.price * item.quantity)
-   let count = Math.round( total.reduce((a, b) => a + b,) * 100)/100;
-    return count;
+    let total = 0;
+     items.forEach((item) => {
+      total +=  (item.price * item.quantity)
+    })
+    return total;
+       
   }
+  //  let count = Math.round( total.reduce((a, b) => a + b,) * 100)/100;
+  
   const CartQty = () =>{
     let itemsQty = items.map((item,index) => + item.quantity)
     let countQty = Math.round( itemsQty.reduce((a, b) => a + b,) * 100)/100;
@@ -20,12 +26,11 @@ function CartTotal({items}) {
       <div className="CartTotal">
         <h1>Subtotal ({CartQty()} Items):
           <span className="CarTotal-price">
-            ${TotalPrice()}
+          <NumberFormat value={TotalPrice()} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} />
           </span>
           <button>Procced Check out</button>
         </h1>
       </div>
     )
 }
-
 export default CartTotal
